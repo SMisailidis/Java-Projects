df69149d23a75f5c50d675489a36437cb1ce0fb8
Savvas Misailidis <savvasmis@hotmail.gr>
2022-12-02 19:54:09 +0200

    fixes

--- BankSimulator/src/Main.java
+++ BankSimulator/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         sql = new SQLiteConnection("Cards.db");
+        
 
         @SuppressWarnings("unused")
         GUISignOrLogIn start = new GUISignOrLogIn();
